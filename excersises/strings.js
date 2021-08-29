473a24f6bb3710aa00319768762cd2de97d430b2
Marta Skrzypczak <mart51w7@stud.kea.dk>
2021-08-29 21:51:38 +0200

    template literals done

---
+++ excersises/strings.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const name = "peter";
+const drink = "pepsi";
+console.log(`Hello ${name}, would you like a ${drink}`);
+console.log("HellO " + name + ", would you like a " + drink + "?");
+
+const myName = "Peter";
+const animal = "Mandu";
+console.log(`My name is ${myName}. 
+I have a cat called ${animal}.`);
+
+console.log(`My name is ${myName}.\nI have a cat called ${animal}.`);
