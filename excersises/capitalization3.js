e96f0238cb0b1f4a8cbc5a86f50f73d765068df9
Marta Skrzypczak <mart51w7@stud.kea.dk>
2021-08-30 16:10:17 +0200

    capitalization of 3th letter works

---
+++ excersises/capitalization3.js
@@ -0,0 +1,26 @@
+/* Given a single name string in an unknown case, e.g. “peter” or “PETER” - 
+create a new string with the same name, where the third letter is uppercase, 
+and the rest is lowercase. I.e. “peTer”.
+
+Hint: use substring, toUpperCase, toLowerCase and simple string concatenation */
+const unknownName = "MARTA";
+const name = unknownName.toLowerCase();
+
+const newName =
+  name.substring(0, 2) + name[2].toUpperCase() + name.substring(3);
+/* name.substring(0, 2) + name[2].toUpperCase() + name.substring(3); */
+console.log(newName);
+
+/* const name = "saSia";
+ 
+const small = name.toLowerCase();
+ 
+let newStr = name[0].toUpperCase() + small.substring(1);
+console.log(newStr);
+ */
+/* const LowerName =
+  UpperName.substring(0, 2) +
+  UpperName[2].toLowerCase() +
+  UpperName.substring(3);
+console.log(LowerName);
+ */
