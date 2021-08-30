b0f8a50086e594d6503be4e2b9f66d6d4cbf3df4
Marta Skrzypczak <mart51w7@stud.kea.dk>
2021-08-30 15:38:58 +0200

    split string into variables

---
+++ excersises/nameparts.js
@@ -0,0 +1,31 @@
+"use strict";
+const name = "Peter Heronimous Hagrid Lind";
+/* const arrayOfNames = name.split(" ");
+const firstName = arrayOfNames[0];
+const middleName = arrayOfNames[1];
+const lastName = arrayOfNames[2];
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
+ */
+
+//Firstname:
+const indexfirst = name.indexOf(" ");
+const firstname = name.substring(0, indexfirst);
+console.log(firstname);
+
+//Middlename:
+const indexlast = name.lastIndexOf(" ");
+const middlename = name.substring(indexfirst + 1, indexlast);
+console.log(middlename);
+
+//Lastname:
+const lastname = name.substring(indexlast + 1);
+console.log(lastname);
+
+console.log(firstName);
+/* 
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
+ */
